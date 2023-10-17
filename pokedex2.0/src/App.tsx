2ecc3161b0b4e07666a709eda69d86aa31131c58
Javier Morón <requin883@gmail.com>
2023-10-17 09:11:41 -0700

    Add pokedex

--- pokedex2.0/src/App.tsx
+++ pokedex2.0/src/App.tsx
@@ -1,7 +1,12 @@
+import { Pokedex } from "./Components/Pokedex";
+
 export default function app() {
   return(
-    <div className="grid place-items-center text-lg">
-    <h1>Hello World</h1>
+    <div className="grid place-items-center bg-amber-200/50">
+      <div className="grid w-[100vw] h-[100vh] place-items-center">
+    <h1 className="font-bold justify-self-center text-[5em]">Pokedex</h1>
+    <Pokedex/>
+      </div>
     </div>
   )
 }
---
+++ pokedex2.0/src/Components/Pokedex.tsx
@@ -0,0 +1,7 @@
+export const Pokedex = () =>{
+  return(
+    <div className="border-black border-[2px]">
+    <h1>hello</h1>
+    </div>
+  )
+}
